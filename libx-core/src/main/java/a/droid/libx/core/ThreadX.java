37efba7a1db145b317117479645b4fd4e676b3dd
HL <justhl@qq.com>
2021-01-14 09:54:56 +0800

    add ThreadX

---
+++ libx-core/src/main/java/a/droid/libx/core/ThreadX.java
@@ -0,0 +1,55 @@
+package a.droid.libx.core;
+
+import android.os.Handler;
+import android.os.Looper;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadX {
+    private enum Type {
+        UI, IO, NEW
+    }
+
+    private final Type type;
+
+    private final Handler handler = new Handler(Looper.getMainLooper());
+    private final ScheduledExecutorService ioThread = Executors.newSingleThreadScheduledExecutor();
+
+    private ThreadX(Type type) {
+        this.type = type;
+    }
+
+    public static ThreadX ui() {
+        return new ThreadX(Type.UI);
+    }
+
+    public void post(Runnable runnable) {
+        if (type == Type.UI) {
+            handler.post(runnable);
+        } else if (type == Type.IO) {
+            ioThread.execute(runnable);
+        } else if (type == Type.NEW) {
+            new Thread(runnable).start();
+        }
+    }
+
+    public void postDelayed(Runnable runnable, long delayMillis) {
+        if (type == Type.UI) {
+            handler.postDelayed(runnable, delayMillis);
+        } else if (type == Type.IO) {
+            ioThread.schedule(runnable, delayMillis, TimeUnit.MILLISECONDS);
+        } else if (type == Type.NEW) {
+            new Thread(() -> {
+                try {
+                    Thread.sleep(delayMillis);
+                    runnable.run();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+    }
+}
